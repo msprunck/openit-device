916deefeeff60d6a5d3af205ed96efd98f14b6d9
msprunck <matthieu@sprunck.com>
2014-03-02 16:06:14 +0100

    Control IO of Raspberry Pi

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+/**
+ * Start the nodejs server with the following command:
+ * OPENIT_DEVICE_ID=<value> OPENIT_KEY=<value> node app.js
+ */
+
 var faye = require('faye');
 var util = require('util');
+var gpio = require("pi-gpio");
 
 /** URL of the OpenIt backend. */
-var URL = 'http://localhost:3000/faye';
+var URL = 'http://openit.appspot.fr/faye';
 
 /** The device ID. */
-var DEVICE_ID = 'ID'; 
+var DEVICE_ID = process.env.OPENIT_DEVICE_ID || 'ID'; 
 
 /** The channel to listen. */
 var CHANNEL = '/control/' + DEVICE_ID;
 
 /** The device key. */
-var KEY = 'KEY';
+var KEY = process.env.OPENIT_KEY || 'KEY';
 
 /****************************************
  ** Subscription to the OpenIt backend. *
@@ -34,6 +40,8 @@ var clientAuth = {
     // Set the auth token
     message.ext.authToken = KEY;
 
+    console.log('Subscription (KEY:' + KEY) + ')';
+
     // Carry on and send the message to the server
     callback(message);
   }
@@ -47,6 +55,7 @@ client.addExtension(clientAuth);
  
 var subscription = client.subscribe(CHANNEL, function(message) {
   console.log("[NEW MESSAGE]: " + util.inspect(message));
+  open();
 });
 
 subscription.callback(function() {
@@ -56,3 +65,23 @@ subscription.callback(function() {
 subscription.errback(function(error) {
   console.log('[SUBSCRIBE FAILED]', error);
 });
+
+/**
+ * Open the door connected to the Raspberry Pi.
+ */
+var open = function(){
+  gpio.open(11, "output", function(err) {  // Open pin 17 for output
+    gpio.write(11, 1, function() {         // Set pin 17 high (1)
+        setTimeout(function(){openRespond()}, 1000);
+    });
+  });
+};
+
+/**
+ * To release the door switch after 1 second.
+ */
+var openRespond = function() {
+  gpio.write(11, 0, function() {            // Set pin 17 high (1)
+        gpio.close(11);                     // Close pin 17
+  });
+}
